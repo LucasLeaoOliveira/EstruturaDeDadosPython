b45cf16741ff94911d5d7dacedd48502b5a47477
Lucas Leão Oliveira <115674066+LucasLeaoOliveira@users.noreply.github.com>
2023-10-16 17:30:20 -0300

    Add files via upload

---
+++ BasicPython/manipulation of variables and constants/main.py
@@ -0,0 +1,23 @@
+numero = -3
+numero_jogos = 14
+numero_convidados = 15
+
+print(numero,numero_jogos,numero_convidados)
+
+pi = 3.14
+numero_euler = 2.71
+escala_terremoto = -4.55
+
+print(numero_euler,escala_terremoto,pi)
+
+letra = 'a'
+palavra1 = 'liguagem de programação'
+palavra2 ='Python'
+
+print(letra,palavra1)
+
+print("Estou aprendendo uma ", palavra1)
+print("Esta ", palavra1,"se chama",palavra2)
+idade = input("digite sua idade")
+print("Sua idade é :" , idade)
+
---
+++ BasicPython/structure for/main.py
@@ -0,0 +1,34 @@
+for i in range(1,6):
+    print(i)
+print("===========================")
+for i in range(5,0,-1):
+    print(i)
+print("===========================")
+
+soma = -15
+for i in range(1,6):
+    print(soma)
+    print(f"+{i}")
+    soma = soma+i
+print(soma)
+
+print("===========================")
+print()
+palavra= 'soverte'
+
+for i in palavra:
+    print(i)
+print()
+print("===========================")
+print()
+for letra in palavra:
+    if letra =='v':
+        print("achou a letra v")
+print("===========================")
+print()
+for i in range (0,5):
+    print(i)
+    print("----")
+    for j in range(0,3):
+        print(j)
+    print()
