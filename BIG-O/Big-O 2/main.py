a848334085dabb0f32ba3ff4b03a82d84e830b38
Lucas Leão Oliveira <115674066+LucasLeaoOliveira@users.noreply.github.com>
2023-10-27 17:29:40 -0300

    Add files via upload

--- BIG-O/Big-O 2/main.py
+++ BIG-O/Big-O 2/main.py
@@ -1,7 +1,6 @@
 # FUNÇÃO 3 DE BIG-O 2
 from timeit import timeit
 
-
 def lista1():
     lista = []
     for i in range(1000):
@@ -13,6 +12,24 @@ def lista1():
 #print("================")
 #print(len(lista1()))
 
+
+
+
 tempo_execucao_funcao = timeit(lambda: lista1(), number=10000)
 
 print(f'Tempo de execução (função): {tempo_execucao_funcao} segundos')
+
+#FUNÇAO 4  BIG-O 2
+
+def lista2():
+    return range(1000)
+
+l = lista2()
+
+#for i in l:
+   # print(i)
+
+tempo_execucao_funcao = timeit(lambda: lista2(), number=10000)
+
+print(f'Tempo de execução (função): {tempo_execucao_funcao} segundos')
+
